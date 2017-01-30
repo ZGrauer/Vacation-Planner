393dfd1f036de50cea791b46aabee3eacc6951a1
ZGrauer <zgrauer@wgu.edu>
2017-01-30 09:28:11 -0600

    docs: Add documentation

--- js/api/wiki.js
+++ js/api/wiki.js
@@ -1,14 +1,28 @@
+/**
+ * @description Represents a Wikipedia API instance
+ */
 var Wiki = function() {
     var self = this;
     this.articleList;
+
+
+    /**
+     * @description Queries Wiki using ajax. When successfull appends data to DOM in
+     * #wiki-div inside the Info Window.  If an error is encountered, a Bootstrap alert
+     * is appended to #wiki-div.
+     * @param  {string} searchStr string to query in Wiki
+     */
     this.getWikiData = function(searchStr) {
-        var wikiUrl = "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchStr + "&format=json&callback=wikiCallback";
-        self.articleList = "";
+        // JSONP url containing query string
+        var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + searchStr + '&format=json&callback=wikiCallback';
+        self.articleList = '';
+        // Ajax call to get info
         $.ajax({
                 url: wikiUrl,
-                dataType: "jsonp",
-                jsonp: "callback"
+                dataType: 'jsonp',
+                jsonp: 'callback'
             })
+            // When successful get data UL and append to Info Window
             .done(function(data, textStatus, jqXHR) {
                 var articles = data[1];
                 var articleDescs = data[2];
@@ -18,14 +32,15 @@ var Wiki = function() {
                         (i < articles.length) && (i !== 3); i++) {
                         self.articleList += '<li><a href="' + articleUrls[i] + '" target="_blank">' + articles[i] + '</a></li>';
                     };
-                    $("#wiki-div").html("<h4>Wiki Articles</h4>" + self.articleList);
+                    $('#wiki-div').html('<h4>Wiki Articles</h4>' + self.articleList);
                 } else {
-                    $("#wiki-div").html("<h4>Wiki Articles</h4>None Found");
+                    $('#wiki-div').html('<h4>Wiki Articles</h4>None Found');
                 }
             })
+            // If failed, append alert to the Info Window
             .fail(function(jqXHR, textStatus, errorThrown) {
-                console.log("Failed to get wikipedia data.<br>" + textStatus + "<br>" + errorThrown);
-                $("#wiki-div").attr("role", "alert").addClass("alert alert-danger").html('' +
+                console.log('Failed to get wikipedia data.<br>' + textStatus + '<br>' + errorThrown);
+                $('#wiki-div').attr('role', 'alert').addClass('alert alert-danger').html('' +
                     '<strong>ERROR!</strong> Failed to get wikipedia data. </div>');
             });
     };
